87de7f6a64987692ad9d0656bacd5e327e8a7a9f
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-17 14:35:10 +0530

    added multiply method

--- Calculator.java
+++ Calculator.java
@@ -8,4 +8,7 @@ class Calculator{
   public static void main(String args[]){ 
     System.out.println(add(2,3));
   }
+  public static int multiply(int num1, int num2){
+    return num1*num2;
+  }
 }
