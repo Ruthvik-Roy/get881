e75627244ad386e6de3edca10635db7ef211af17
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-18 15:31:29 +0530

    removed divide method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -13,10 +13,6 @@ public class Calc {
 		System.out.println("Multiplication = " + (num1 * num2));
 	}
 	
-	public void divide(int num1, int num2) {
-		System.out.println("Division = " + (num1 / num2));
-	}
-
 	public void message() {
 		System.out.println("Hello");
 	}
